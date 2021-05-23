2dcb1545d1241e8a8908fc32e2452e9403c449fd
vugar005 <abdullayev1.618@hotmail.com>
2021-05-23 18:21:12 +0200

    Common regex methods

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
+// Write RegEx!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1> Regular Expressions </h1>`;
 
-const regTest = /hello/;
-console.log(regTest.test('hello there'));
+const regex1 = /JS/;
+const textInput = 'Front End JS developer';
 
+console.log(regex1.test(textInput));
+
+// exec returns array (pattern, index, input)
+console.log(regex1.exec(textInput));
+
+// match
+// We can use string match to test RegEx
+// It returns same data as exec on regex1
+
+console.log(textInput.match(regex1));
+
+// search
+// returns index of matched pattern
+console.log(textInput.search(regex1));
+
+// replace
+// replace text with some other text
+// Ex: 'hello there' to 'hi there'
+// returns new string. Does not modify original string
+console.log(textInput.replace(regex1, 'Angular'));
+
+// split
+// Splits text input into parts. First part before patten matched
+// second part after pattern matched
+console.log(textInput.split(regex1));
